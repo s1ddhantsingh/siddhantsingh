8256660e76cfe228ee79eb926e45ed45c210e1ea
Siddhant Singh <singhsiddhantakshat@gmail.com>
2023-06-03 11:34:50 -0500

    made more updates

--- next-blog-test/src/app/page.js
+++ next-blog-test/src/app/page.js
@@ -5,24 +5,34 @@ import Link from "next/link";
 const Home = () => {
 	return (
 		<>
-			<Image
-				priority
-				src="/images/profile.jpeg"
-				height={100}
-				width={100}
-				alt="siddhant singh profile image"
-				className=""
-			/>
-			<h1>siddhant singh</h1>
-			<h3>
-				hello there, my name is sidd. i'm a startup founder, software engineer,
-				and a student.
-			</h3>
-			<h2>
-				reach me on <a href="https://twitter.com/siddhants1ngh">twitter</a>,{" "}
-				<a href="https://www.linkedin.com/in/siddhant-singh-0217/">linkedin</a>,
-				or by singhsiddhantakshat@gmail.com.
-			</h2>
+			<div className="grid justify-items-center p-4 m-10 bg-slate-400 rounded-lg">
+				<Image
+					priority
+					src="/images/profile.jpeg"
+					height={100}
+					width={200}
+					alt="siddhant singh profile image"
+					className="rounded-full"
+				/>
+				<h1 className="text-2xl">siddhant singh</h1>
+				<h3 className="text-lg">
+					hello there, my name is sidd. i'm a startup founder, software
+					engineer, and a student.
+				</h3>
+				<h2 classname="text-xs">
+					reach me on <a href="https://twitter.com/siddhants1ngh">twitter</a>,{" "}
+					<a href="https://www.linkedin.com/in/siddhant-singh-0217/">
+						linkedin
+					</a>
+					, or by singhsiddhantakshat@gmail.com.
+				</h2>
+			</div>
+			<div className="grid justify-items-center p-4 m-10 bg-slate-400 rounded-lg">
+				blog 1
+			</div>
+			<div className="grid justify-items-center p-4 m-10 bg-slate-400 rounded-lg">
+				blog 1
+			</div>
 		</>
 	);
 };
