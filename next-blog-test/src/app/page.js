dee31b4e2989c9aacece570671c0b67a2cf065a2
Siddhant Singh <singhsiddhantakshat@gmail.com>
2023-06-04 00:11:43 -0500

    +1

--- next-blog-test/src/app/page.js
+++ next-blog-test/src/app/page.js
@@ -2,6 +2,21 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+const data = [
+	{
+		text: "blog title 1",
+		body: "Lorem ipsum dolor sit amet, ut simul dicant eum. Id tantas deleniti complectitur vix, nominati consequuntur usu cu. Vel quidam saperet ne, appetere concludaturque usu ad. Ut his dicunt pertinax, vim iusto populo alterum eu. Has cu deleniti dignissim. Commodo repudiandae disputationi at eos, duo et modus possit. In purto elaboraret persequeris mea, doctus vituperata nam et. Eam wisi similique ex, falli tacimates maiestatis vis at. No vim partem legere volumus, per no solum dictas concludaturque. Ubique iuvaret argumentum in his, pertinax quaestio instructior vix cu. Sit te tation partem, id vix modus oporteat disputationi. Pro cu case laudem delicata.",
+	},
+	{
+		text: "blog title 2",
+		body: "Lorem ipsum dolor sit amet, ut simul dicant eum. Id tantas deleniti complectitur vix, nominati consequuntur usu cu. Vel quidam saperet ne, appetere concludaturque usu ad. Ut his dicunt pertinax, vim iusto populo alterum eu. Has cu deleniti dignissim. Commodo repudiandae disputationi at eos, duo et modus possit. In purto elaboraret persequeris mea, doctus vituperata nam et. Eam wisi similique ex, falli tacimates maiestatis vis at. No vim partem legere volumus, per no solum dictas concludaturque. Ubique iuvaret argumentum in his, pertinax quaestio instructior vix cu. Sit te tation partem, id vix modus oporteat disputationi. Pro cu case laudem delicata.",
+	},
+	{
+		text: "blog title 3",
+		body: "Lorem ipsum dolor sit amet, ut simul dicant eum. Id tantas deleniti complectitur vix, nominati consequuntur usu cu. Vel quidam saperet ne, appetere concludaturque usu ad. Ut his dicunt pertinax, vim iusto populo alterum eu. Has cu deleniti dignissim. Commodo repudiandae disputationi at eos, duo et modus possit. In purto elaboraret persequeris mea, doctus vituperata nam et. Eam wisi similique ex, falli tacimates maiestatis vis at. No vim partem legere volumus, per no solum dictas concludaturque. Ubique iuvaret argumentum in his, pertinax quaestio instructior vix cu. Sit te tation partem, id vix modus oporteat disputationi. Pro cu case laudem delicata.",
+	},
+];
+
 const Home = () => {
 	return (
 		<>
@@ -27,12 +42,16 @@ const Home = () => {
 					, or by singhsiddhantakshat@gmail.com.
 				</h2>
 			</div>
-			<div className="grid justify-items-center p-4 m-10 bg-slate-400 rounded-lg">
-				blog 1
-			</div>
-			<div className="grid justify-items-center p-4 m-10 bg-slate-400 rounded-lg">
-				blog 1
-			</div>
+			{data.map((e) => {
+				return (
+					<div className="grid justify-items-center p-4 m-10 bg-slate-400 rounded-lg">
+						<div className="bg-slate-300 rounded-lg p-2 m-2 bg-left-top">
+							{e.text}
+						</div>
+						<p>{e.body}</p>
+					</div>
+				);
+			})}
 		</>
 	);
 };
